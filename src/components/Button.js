b098ec247e637dfbb2d014aa4c7990aa5f55db75
Asim <asim.awan@outlook.com>
2021-11-15 20:58:45 -0500

    Create and refactor Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,19 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from "classnames";
+
 export default function Button(props) {
-   return <></>;
+
+  let buttonClass = classNames({
+    button: true,
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return(
+    <>
+    <button onClick={props.onClick} className={buttonClass} disabled={props.disabled}>{props.children}</button>
+    </>
+   );
 }
