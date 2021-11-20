e2a9bfa88304f5603fbce873b1003b103b1a9117
Asim <asim.awan@outlook.com>
2021-11-20 12:07:38 -0500

    Perform minor refactoring

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "components/Application.scss";
 
@@ -25,10 +25,12 @@ const days = [
 
 
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
@@ -38,8 +40,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
           days = {days}
-          day = {"Monday"}
-          setDay = {day => console.log(day)}
+          value = {day}
+          onChange = {setDay}
           />
         </nav>
         <img
