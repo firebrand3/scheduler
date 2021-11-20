d3be27b4344bcd55795653c8b01260035ac9f1b5
Asim <asim.awan@outlook.com>
2021-11-20 12:09:14 -0500

    Refactor DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,15 +3,12 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  
   const listOfDays = props.days.map(nowday => {
     return <DayListItem
         {...nowday}
         key={nowday.id}
-        // name={nowday.name} 
-        // spots={nowday.spots} 
-        selected={nowday.name === props.day}
-        setDay={props.setDay}  
+        selected={nowday.name === props.value}
+        setDay={() => props.onChange(props.name)}
     />})
 
   return (
