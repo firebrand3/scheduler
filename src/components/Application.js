a6956e6ff6c47709d20aeff506502db6e89e0220
Asim <asim.awan@outlook.com>
2021-11-23 00:15:48 -0500

    Remove Days array and fetch Days data from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,10 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
 
 const appointments = {
   "1": {
@@ -65,7 +47,16 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState("");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days")
+      .then(res => {
+        setDays(res.data)
+      })
+}, [])
+
 
   const arrayOfAppointments = Object.values(appointments).map((appointment) => {
     return (
