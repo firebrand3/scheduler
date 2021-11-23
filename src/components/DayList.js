1f16f7052fce566bb578f3cd3956f000c7238f74
Asim <asim.awan@outlook.com>
2021-11-23 00:15:09 -0500

    Refactor to remove error in setDay prop

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,12 +3,12 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const listOfDays = props.days.map(nowday => {
+  const listOfDays = props.days.map(day => {
     return <DayListItem
-        {...nowday}
-        key={nowday.id}
-        selected={nowday.name === props.value}
-        setDay={() => props.onChange(props.name)}
+        {...day}
+        key={day.id}
+        selected={day.name === props.value}
+        setDay={() => props.onChange(day.name)}
     />})
 
   return (
