4a351009cddaf951abf66a1abc2c1eb29dd0f828
Asim <asim.awan@outlook.com>
2021-11-27 18:15:08 -0500

    Add getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,28 +1,33 @@
 export function getAppointmentsForDay(state, day) {
-  // console.log("STATE!!", state)
   const appointmentsForDay = [];
-  
-  if(!state.days.length) {
-    return [];
-  } 
-  
-  const filteredDays = state.days.filter(stateDay => stateDay.name === day);
-  
-  // console.log("RESULT!!", filteredDays[0].appointments);
 
-  if(!filteredDays.length){
+  if (!state.days.length) {
+    return [];
+  }
+
+  const filteredDays = state.days.filter((stateDay) => stateDay.name === day);
+  if (!filteredDays.length) {
     return [];
   }
 
   const appoitmentArray = filteredDays[0].appointments;
 
   for (const appointment of appoitmentArray) {
-    if(state.appointments[appointment]) {
-      appointmentsForDay.push(state.appointments[appointment])
-      // console.log("*****", appointmentsForDay)
+    if (state.appointments[appointment]) {
+      appointmentsForDay.push(state.appointments[appointment]);
     }
-
   }
 
   return appointmentsForDay;
+}
+
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  return {
+    student: interview.student,
+    interviewer: { ...state.interviewers[interview.interviewer] },
+  };
 }
