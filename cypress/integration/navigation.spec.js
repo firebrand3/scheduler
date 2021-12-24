bb536d45bd6d93f4ed4ec18325ab4d3e208a8a8a
Asim <asim.awan@outlook.com>
2021-12-24 17:13:15 -0500

    Remove comments

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,8 +1,6 @@
 describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    // cy.contains('Tuesday').click();
-    // cy.get("li").contains("Tuesday").click();
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
       .should("have.class", "day-list__item--selected");
