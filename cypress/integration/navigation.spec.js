b75cdfa5a4d02c3f07a6c866d6ad2fa22bc0d89a
Asim <asim.awan@outlook.com>
2021-12-24 17:11:37 -0500

    Add cypress test for navigating days

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,10 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+  it("should navigate to Tuesday", () => {
     cy.visit("/");
+    // cy.contains('Tuesday').click();
+    // cy.get("li").contains("Tuesday").click();
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
